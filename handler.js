ccaf66d38f6a5e0427af4b4e114eebf47043d509
Masao Kitamura <masaok@gmail.com>
2019-06-15 20:44:18 -0700

    Node server works standalone

--- handler.js
+++ handler.js
@@ -1,14 +1,20 @@
-'use strict';
+import { graphqlLambda, graphiqlLambda } from 'apollo-server-lambda';
+import { makeExecutableSchema } from 'graphql-tools';
+import { schema } from './schema';
+import { resolvers } from './resolvers';
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+const myGraphQLSchema = makeExecutableSchema({
+  typeDefs: schema,
+  resolvers,
+});
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+exports.graphqlHandler = function graphqlHandler(event, context, callback) {
+  function callbackWithHeaders(error, output) {
+    // eslint-disable-next-line no-param-reassign
+    output.headers['Access-Control-Allow-Origin'] = '*';
+    callback(error, output);
+  }
+
+  const handler = graphqlLambda({ schema: myGraphQLSchema });
+  return handler(event, context, callbackWithHeaders);
 };
